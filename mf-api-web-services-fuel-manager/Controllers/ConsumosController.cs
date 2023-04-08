0e6568a2a9342a31530bc7322b90099d38e1cf38
Oswaldo F Silva <oswaldo_f@yahoo.com.br>
2023-04-08 14:10:02 -0300

    Add project files.

---
+++ mf-api-web-services-fuel-manager/Controllers/ConsumosController.cs
@@ -0,0 +1,86 @@
+﻿using mf_api_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_api_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumosController : ControllerBase
+    {
+       
+        private readonly AppDbContext _context;
+
+        public ConsumosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Consumos.ToListAsync();
+            return Ok(model);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Consumo model)
+        {
+
+            _context.Consumos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Consumos.
+                FirstOrDefaultAsync(c => c.Id == id);
+
+            if (model == null) return NotFound();
+
+            GerarLinks(model);
+
+            return Ok(model);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Consumo model)
+        {
+            if (id != model.Id) return BadRequest();
+
+            var modelDb = await _context.Consumos.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (modelDb == null) return NotFound();
+
+            _context.Consumos.Update(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _context.Consumos.FindAsync(id);
+
+            if (model == null) return NotFound();
+
+            _context.Consumos.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+        private void GerarLinks(Consumo model)
+        {
+            model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "self", metodo: "GET"));
+            model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "update", metodo: "PUT"));
+            model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "DELETE"));
+        }
+
+    }
+}
